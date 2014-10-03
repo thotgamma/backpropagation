3b7f7d1bc6f8596ea7607ad4d4056aa28f161542
thotgamma <thotgamma@GammaBook.local>
2014-10-03 21:04:00 +0900

    Initial Commit

---
+++ bp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bp
+//
+//  Created by thotgamma on 2014/10/03.
+//  Copyright (c) 2014年 thotgamma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
