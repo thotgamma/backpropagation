cc69aea4241d303d4c093a5433fecb8c6aacacd3
thotgamma <thotgamma@GammaBook.local>
2014-10-04 04:32:56 +0900

    -

--- bp/main.cpp
+++ bp/main.cpp
@@ -14,33 +14,22 @@ int main(int argc, const char * argv[]) {
     
     neuralNetwork nnet;
     nnet.makeNewNetwork();
-    nnet.learn("input_1.txt", 0.3, 500000);
     
-    /*
-    nnet.inputCore[1] = 0.968627;
-    nnet.inputCore[2] = 0.988235;
-    nnet.inputCore[3] = 0.701961;
+    nnet.learn("input.txt", 0.5, 5000);
+    
+    nnet.inputCore[1] = 0;
     nnet.advanceCalc();
-    std::cout << "0.155714:" << nnet.outputCore[0] << std::endl;
+    std::cout << "calc 0:" << nnet.outputCore[0] << std::endl;
+    nnet.resetCores();
+    nnet.inputCore[1] = 0.5;
+    nnet.advanceCalc();
+    std::cout << "calc 0.5:" << nnet.outputCore[0] << std::endl;
+    nnet.resetCores();
+    nnet.inputCore[1] = 1;
+    nnet.advanceCalc();
+    std::cout << "calc 1:" << nnet.outputCore[0] << std::endl;
     nnet.resetCores();
     
-    nnet.inputCore[1] = 0.105882;
-    nnet.inputCore[2] = 0.356863;
-    nnet.inputCore[3] = 0.823529;
-    nnet.advanceCalc();
-    std::cout << "0.829286:" << nnet.outputCore[0] << std::endl;
-    nnet.resetCores();
-    
-    nnet.inputCore[1] = 0.964706;
-    nnet.inputCore[2] = 0.968627;
-    nnet.inputCore[3] = 0.498039;
-    nnet.advanceCalc();
-    std::cout << "0.0707143:" << nnet.outputCore[0] << std::endl;
-    nnet.resetCores();
-     */
-    
-    
-    nnet.save("a");
     
     return 0;
 }
